459aa78d59c27286ab46d8b27ff87fab50720cb6
renuaayush <111826804+renuaayush@users.noreply.github.com>
2024-04-27 07:05:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
-// packages
+
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
-
-// Utiles
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -18,7 +17,11 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+app.use(cors({
+  origin: "",
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -34,6 +37,8 @@ app.get("/api/config/paypal", (req, res) => {
 });
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
+
+
